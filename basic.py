455a5bc4ef034dee8afcc1e269f21373db0909f8
Mark Lescroart <mark.lescroart@gmail.com>
2015-09-06 21:40:29 -0700

    Moved main functions to basic.py, some updates, long overdue commit

---
+++ basic.py
@@ -0,0 +1,199 @@
+"""Basic module of data generation functions."""
+import numpy as _np
+from scipy.stats import zscore 
+from scipy import linalg as la
+import os
+
+boilerplate_file = os.path.join(os.path.split(__file__)[0],'boilerplate.py')
+
+def from_x(X,wts,signal_corr=1.0,do_zscore=True):
+	"""Make fake data from some design matrix + weights on each channel in it.
+
+	Builds a fake data matrix from some source matrices. Computes `X.dot(wts)`
+	and adds noise such that the output (`Y`) is correlated with X.dot(wts) by 
+	(approximately) `signal_corr`.
+
+	Size, std, sparsity of output are all determined by X, wts. 
+	
+	Parameters
+	----------
+	X : array-like, t x chan
+		Design matrix for fake data. Each column in `X` is a feature / factor / channel that 
+		influences the final data according to the weight assigned to that channel by `wts`
+	wts : array-like, chan x n
+		Weights for each of <chan> columns in  X. If wts is 1D, returns a single Y; 
+		if n > 1, returns fake data for n samples (/ voxels / repeats)
+	signal_corr : scalar or vector | 1.0-> SCALAR ONLY FOR NOW??
+		Desired signal level (correlation with perfect X.dot(w) matrix) If a vector is provided, 
+		it should be the same length as n (2nd dimension of wts) to specify 
+	do_zscore : bool | True
+
+	Returns
+	-------
+	Y : array-like, t x n
+	
+	Notes
+	-----
+	TO DO: 
+	- Return class instance instead of just matrix...
+	- store original X, w, signal_corr, sparsity, etc (???)
+
+	"""
+	## -- Input handling -- ##
+	if not isinstance(wts,_np.ndarray):
+		wts = _np.array(wts)
+	if wts.ndim==1:
+		wts = wts[:,None] # make into a 2D array
+	t,nCh1 = X.shape
+	nCh2,nVox = wts.shape
+	## -- Add noise -- ##
+	signal = X.dot(wts) # if signal_corr is a vector, does this work??
+	sig_std = _np.std(signal,axis=0)
+	noise = _np.random.randn(t,nVox)*sig_std # if signal_corr is a vector, does this work??
+	# From http://www.sitmo.com/article/generating-correlated-random-numbers/
+	dat = signal*signal_corr+noise*_np.sqrt(1-signal_corr**2)
+	## -- Optionally zscore -- ##
+	if do_zscore:
+		dat = zscore(dat)
+	return dat
+
+def from_matrix(data,sz,signal_corr=1.0,corr_falloff=None,do_zscore=True):
+	"""Generate fake data such that columns share variance w/ input matrix
+	
+	Useful for generating dependent design matrices for fake regressions.
+
+	Parameters
+	----------
+	data : array-like, m x n
+		Input matrix with which to correlate generated fake data
+
+	sz : tuple (x,y)
+		(x,y) - number of columns in x and y variables
+		x should always be >= y
+	nt : scalar
+		length of 1st dim (time), same for both
+	corr_falloff : vector (1D array) of monotonically decreasing values (1->0)
+		allows some channels to be less correlated than others. Does not have
+		to go all the way to zero. Default = all ones.
+	do_zscore = bool | True
+		whether to z score all columns of output matrix
+	
+	Notes
+	-----
+	[]
+	"""
+	## -- Input handling -- ##
+	m_in,n_in = data.shape
+	m_out,n_out = sz
+	if n_out>n_in or m_out>m_in:
+		raise Exception('Output shape must be <= shape of input data in all dimensions')
+	if corr_falloff is None:
+		corr_falloff = _np.ones((n_out))
+	# Singular value decomposition to get underlying structure of input data matrix
+	U,S,Vt = la.svd(data)
+	sn = len(S)
+	# U is m_in x m_in (= m_out x m_out)
+	# Sm is sn x xn (sn = min(m_in,n_in))
+	# V is n_in x n_in (!= n_out x n_out) -> Needs to be n_out x n_out
+	# Shuffle columns
+	Vtx = _np.random.randn(n_out,n_out)
+	Sm = _np.diag(S)
+	if sn < m_out:
+		Sm = _np.vstack((Sm,_np.zeros((m_out-sn,sn))))
+	if sn < n_out:
+		Sm = _np.hstack((Sm,_np.zeros((sn,n_out-sn))))
+	# Create base signal
+	signal = U.dot(Sm).dot(Vtx)
+	# Add noise
+	sig_std = _np.std(signal,axis=0)
+	noise = _np.random.randn(m_out,n_out)*sig_std 
+	# if signal_corr is a vector, does this work??
+	# From http://www.sitmo.com/article/generating-correlated-random-numbers/
+	X = signal*signal_corr+noise*_np.sqrt(1-signal_corr**2)
+	## -- Optionally zscore -- ##
+	if do_zscore:
+		X = zscore(X)
+	return X
+
+
+def from_svd(S,m,n,U=None,V=None,do_zscore=True):
+	"""Generate matrix based on U,S,V matrices as in Singular Value Decomposition
+
+	This is a useful way to generate a random matrix with some degree of internal structure (i.e., 
+	some amount of correlation between columns or rows). 
+
+	Parameters
+	----------
+	S : Singular values for matrix
+	m : scalar
+		number of rows of matrix
+	n : scalar
+		number of columns of matrix
+	U : array-like | None
+		Eigenvectors for row covariance for matrix. If `U`=None, a random ( N~(0,1) ) matrix is used.
+	V : array-like | None
+		Eigenvectors for column covariance for matrix. If `V`=None, a random ( N~(0,1) ) matrix is used.
+
+	Returns
+	-------
+	dat : array-like
+		m x n array (U*S*V')
+
+	Notes
+	-----
+	Are there ways to add more constraints here?? (Specific sparsity? from_x like behavior?)
+	Possibly use this function in from_matrix??
+	"""
+	S = _np.diag(S)
+	if m>n:
+		S = _np.pad(S,[(0,m-n),(0,0)],mode='constant')
+	elif n>m:
+		S = _np.pad(S,[(0,0),(0,n-m)],mode='constant')
+	if U is None:
+		# Need orthogonal matrix, such that U.T.dot(U) = U.dot(U.T) = Identity   sqrtm, inv
+		U = _np.random.randn(m,m)
+		U = U.dot(la.inv(la.sqrtm(U.T.dot(U))))
+		#print("U orthogonality test:") # works. off-diagonal are <10e-14, diagonal values are ~1
+		#print(U.T.dot(U))
+	if V is None:
+		V = _np.random.randn(n,n)
+		V = V.dot(la.inv(la.sqrtm(V.T.dot(V))))
+		#print("V orthogonality test:")
+		#print(V.T.dot(V))
+	M = U.dot(S).dot(V.T)
+	if do_zscore:
+		M = zscore(M)
+	return M
+
+def get_correlated_signal(x,signal_corr=0.5,noise=None,do_zscore=False):
+	"""Generate a vector or matrix with the columns correlated with the input by (approximately) `signal_corr`
+
+	Method taken from http://www.sitmo.com/article/generating-correlated-random-numbers/
+	"""
+	if noise is None:
+		sig_std = _np.nanstd(x,axis=0)
+		noise = _np.random.randn(*x.shape)*sig_std 
+	# if signal_corr is a vector, does this work??
+	y = x*signal_corr+noise*_np.sqrt(1-signal_corr**2)
+	if do_zscore:
+		y = zscore(y)
+	return y
+
+def add_noise_cols(X,n):
+	"""Add noise columns to a matrix.
+
+	Add `n` columns of noise channels to the right of array `X`
+	
+	Parameters
+	----------
+	X : array-like
+		Original data
+	n : scalar
+		Number of noise columns to add
+	
+	TO DO:
+	Add parameters to allow addition columns correlated with extant values of X
+	Look into _np.pad (with padding function) to do this...
+	"""
+	return _np.hstack((X,_np.random.randn(X.shape[0],n)))
+
--- fmri.py
+++ fmri.py
@@ -13,9 +13,105 @@ TO DO:
 (All the matlab shit that went into previous work...)
 
 """
+import numpy as np
+import scipy.stats
+import warnings
 
+def hrf(shape='twogamma', sf=1, pttp=5, nttp=15, pnr=6, ons=0, pdsp=1, ndsp=1, s=None, d=0):
+	"""Create canonical hemodynamic response filter
+
+	Parameters
+	----------
+	shape : 
+		HRF general shape {'twogamma' [, 'boynton']}
+	sf : 
+		HRF sample frequency (default: 1s/16, OK: [1e-3 .. 5])
+	pttp : 
+		time to positive (response) peak (default: 5 secs)
+	nttp : 
+		time to negative (undershoot) peak (default: 15 secs)
+	pnr : 
+		pos-to-neg ratio (default: 6, OK: [1 .. Inf])
+	ons : 
+		onset of the HRF (default: 0 secs, OK: [-5 .. 5])
+	pdsp : 
+		dispersion of positive gamma PDF (default: 1)
+	ndsp : 
+		dispersion of negative gamma PDF (default: 1)
+	s : 
+		sampling range (default: [0, ons + 2 * (nttp + 1)])
+	d : 
+		derivatives (default: 0)
 	
+	Returns
+	-------
+	h : HRF function given within [0 .. onset + 2*nttp]
+	s : HRF sample points
+	
+	Notes
+	-----
+	The pttp and nttp parameters are increased by 1 before given
+	as parameters into the scipy.stats.gamma.pdf function (which is a property
+	of the gamma PDF!)
 
+	Converted to python from BVQXtools 
+	Version:  v0.7f
+	Build:    8110521
+	Date:     Nov-05 2008, 9:00 PM CET
+	Author:   Jochen Weber, SCAN Unit, Columbia University, NYC, NY, USA
+	URL/Info: http://wiki.brainvoyager.com/BVQXtools
+	"""
+
+	# Input checks
+	if sf > 5:
+	    sf = 1/16
+	elif sf < 0.001:
+	    sf = 0.001
+	if not shape.lower() in ('twogamma', 'boynton'):
+		warnings.warn('Shape can only be "twogamma" or "boynton"')
+		shape = 'twogamma'
+	if s is None:
+	    s = np.arange(0,(ons + 2 * (nttp + 1)), sf) - ons
+	else:
+	    s = np.arange(np.min(s),np.max(s),sf) - ons;
+
+	# computation (according to shape)
+	h = np.zeros((len(s), d + 1));
+	if shape.lower()=='boynton':
+	    # boynton (single-gamma) HRF
+		h[:,0] = scipy.stats.gamma.pdf(s, pttp + 1, pdsp)
+		if d > 0:
+			raise NotImplementedError('Still WIP - code just needs porting.')
+			"""# Matlab code, partly translated:
+			h[:, 1] = h[:, 1] - scipy.stats.gamma.pdf(s + 1, pttp + 1, pdsp);
+			hi = find(h[:, 1] ~= 0);
+			h[hi, 1] = h[hi, 1] - ((pinv(h[hi, 1]' * h[hi, 1]) * h[hi, 1]' * h[hi, 1])' * h[hi, 1]')';
+			if d > 1:
+				h[:,2] = h[:, 1] - scipy.stats.gamma.pdf(s, pttp + 1, pdsp / 1.01);
+				hi = find(h[:,2] ~= 0);
+				h[hi,2] = h[hi,2] - ((pinv(h[hi, [1, 2]).T * h[hi, [1, 2])) * h[hi, [1, 2]).T * h[hi,2]).T * h[hi, [1, 2]).T).T;"""
+	elif shape.lower()=='twogamma':
+		gpos = scipy.stats.gamma.pdf(s, pttp + 1, pdsp)
+		gneg = scipy.stats.gamma.pdf(s, nttp + 1, ndsp) / pnr
+		h[:,0] =  gpos-gneg	            
+		if d > 0:
+			raise NotImplementedError('Still WIP. Sorting through matlab multiplications is annoying.')
+			"""gpos = scipy.stats.gamma.pdf(s-1, pttp + 1, pdsp)
+			gneg = scipy.stats.gamma.pdf(s-1, nttp + 1, ndsp) / pnr
+			h[:, 1] = h[:, 0] - gpos - gneg
+			hi = h[:, 1] != 0
+			h[hi, 1] = h[hi, 0] - ((np.linalg.pinv(h[hi, 0].T * h[hi, 0]) * h[hi, 0].T * h[hi, 0]).T * h[hi, 1].T).T
+			if d > 1:
+				h[:,2] = (h[:, 1] - (scipy.stats.gamma.pdf(s, (pttp + 1) / 1.01, pdsp / 1.01) - scipy.stats.gamma.pdf(s, nttp + 1, ndsp) / pnr)) / 0.01;
+				hi = h[:,2] != 0
+				h[hi,2] = h[hi,2] - ((pinv(h[hi, [1, 2]).T * h[hi, [1, 2])) * h[hi, [1, 2]).T * h[hi,2]).T * h[hi, [1, 2]).T).T;
+			"""
+	# normalize for convolution
+	if d < 1:
+		h /= np.sum(h)
+	else:
+		h /= np.tile(np.sqrt(np.sum(h**2)), h.shape[0], 1)
+	return s,h
 
 def rep_n(X,n):
 	return np.tile(X,(n,1)).T.flatten()
